e60b514272e2505bb36781eae67a170eda99954a
Donald33 Wang <donald33wang@gmail.com>
2023-11-27 09:46:55 +0800

    Chores

--- src/st/LinearProbingHashST.java
+++ src/st/LinearProbingHashST.java
@@ -1,99 +1,100 @@
 package st;
 
 public class LinearProbingHashST<Key, Value> {
-  private int N;
-  private int M;
-  private Key[] keys;
-  private Value[] vals;
 
-  public LinearProbingHashST() {
-    this(16);
-  }
+    private int N;
+    private int M;
+    private Key[] keys;
+    private Value[] vals;
 
-  public LinearProbingHashST(int M) {
-    this.M = M;
-    keys = (Key[]) new Object[M];
-    vals = (Value[]) new Object[M];
-  }
-
-  public void put(Key key, Value val) {
-    if (N >= M / 2) {
-      resize(2 * M);
+    public LinearProbingHashST() {
+        this(16);
     }
 
-    int i;
-    for (i = hash(key); keys[i] != null; i = (i + 1) % M) {
-      if (keys[i].equals(key)) {
+    public LinearProbingHashST(int M) {
+        this.M = M;
+        keys = (Key[]) new Object[M];
+        vals = (Value[]) new Object[M];
+    }
+
+    public void put(Key key, Value val) {
+        if (N >= M / 2) {
+            resize(2 * M);
+        }
+
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
+            }
+        }
+
+        keys[i] = key;
         vals[i] = val;
-        return;
-      }
+        N++;
     }
 
-    keys[i] = key;
-    vals[i] = val;
-    N++;
-  }
+    public Value get(Key key) {
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                return vals[i];
+            }
+        }
 
-  public Value get(Key key) {
-    for (int i = hash(key); keys[i] != null; i = (i + 1) % M) {
-      if (keys[i].equals(key)) {
-        return vals[i];
-      }
+        return null;
     }
 
-    return null;
-  }
+    public void delete(Key key) {
+        if (!contains(key)) {
+            return;
+        }
 
-  public void delete(Key key) {
-    if (!contains(key)) {
-      return;
+        int i = hash(key);
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % M;
+        }
+
+        keys[i] = null;
+        vals[i] = null;
+
+        i = (i + 1) % M;
+        while (keys[i] != null) {
+            Key keyToRedo = keys[i];
+            Value valToRedo = vals[i];
+            keys[i] = null;
+            vals[i] = null;
+            N--;
+            put(keyToRedo, valToRedo);
+            i = (i + 1) % M;
+        }
+
+        N--;
+        if (N > 0 && N == M / 8) {
+            resize(M / 2);
+        }
     }
 
-    int i = hash(key);
-    while (!key.equals(keys[i])) {
-      i = (i + 1) % M;
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
     }
 
-    keys[i] = null;
-    vals[i] = null;
+    private void resize(int cap) {
+        LinearProbingHashST<Key, Value> t;
+        t = new LinearProbingHashST<Key, Value>(cap);
 
-    i = (i + 1) % M;
-    while (keys[i] != null) {
-      Key keyToRedo = keys[i];
-      Value valToRedo = vals[i];
-      keys[i] = null;
-      vals[i] = null;
-      N--;
-      put(keyToRedo, valToRedo);
-      i = (i + 1) % M;
+        for (int i = 0; i < M; i++) {
+            if (keys[i] != null) {
+                t.put(keys[i], vals[i]);
+            }
+        }
+
+        keys = t.keys;
+        vals = t.vals;
+        M = t.M;
     }
 
-    N--;
-    if (N > 0 && N == M / 8) {
-      resize(M / 2);
+    private boolean contains(Key key) {
+        return get(key) != null;
     }
-  }
-
-  private int hash(Key key) {
-    return (key.hashCode() & 0x7fffffff) % M;
-  }
-
-  private void resize(int cap) {
-    LinearProbingHashST<Key, Value> t;
-    t = new LinearProbingHashST<Key, Value>(cap);
-
-    for (int i = 0; i < M; i++) {
-      if (keys[i] != null) {
-        t.put(keys[i], vals[i]);
-      }
-    }
-
-    keys = t.keys;
-    vals = t.vals;
-    M = t.M;
-  }
-
-  private boolean contains(Key key) {
-    return get(key) != null;
-  }
 }
--- src/st/RedBlackBST.java
+++ src/st/RedBlackBST.java
@@ -1,18 +1,23 @@
 package st;
 
 public class RedBlackBST<Key extends Comparable<Key>, Value> {
-  private Node root;
 
-  private class Node {
-    private Key key;
-    private Value val;
-    private Node left, right;
-    private int N;
+    private Node root;
 
-    public Node(Key key, Value val, int N) {
-      this.key = key;
-      this.val = val;
-      this.N = N;
+    private class Node {
+
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+
+        public void put() {
+        }
     }
-  }
 }
